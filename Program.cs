0c3ee86ed8e9a51ecd14aea7c728387dba685bbb
syed-minhaj <minhajsulmuneeb@gmail.com>
2026-01-12 06:19:58 +0500

    first comment, feat: uploading video

---
+++ Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Http.Features;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+builder.Services.Configure<FormOptions>(options =>
+{
+    options.MultipartBodyLengthLimit = 5000000000; // 5GB
+});
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.MaxRequestBodySize = 5L * 1024 * 1024 * 1024; // 5 GB
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+}
+
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/", () =>
+{
+    
+    return "hello";
+});
+
+app.MapPost("/api/video/upload", async (HttpRequest request) =>
+{
+    if (!request.HasFormContentType)
+        return Results.BadRequest("Missing form content type");
+
+    var form = await request.ReadFormAsync();
+    var file = form.Files["file"];
+
+    if (file == null || file.Length == 0)
+        return Results.BadRequest("No file found");
+    
+    if (!file.ContentType.Contains("mp4"))
+        return Results.BadRequest("Invalid file type");
+    
+    if (file.Length > 5_000_000_000)
+        return Results.BadRequest("File too large");
+
+    string videoId = await saveFile.saveFileAsync(file);
+
+    return Results.Ok( new
+        {
+            nessage = "Success",
+            videoId = videoId
+        }
+    );
+});
+
+app.Run();
---
+++ utils/saveFile.cs
@@ -0,0 +1,20 @@
+
+
+class saveFile
+{
+    public static async Task<string> saveFileAsync(IFormFile  file)
+    {
+        var videoId = Guid.NewGuid().ToString();
+
+        var uploadDir = Path.Combine(Directory.GetCurrentDirectory(), "storage" , "uploads");
+        Directory.CreateDirectory(uploadDir);
+        
+        var filePath = Path.Combine(uploadDir, $"{videoId}.mp4");
+        
+
+        await using var stream = new FileStream(filePath, FileMode.Create);
+        await file.CopyToAsync(stream);
+
+        return videoId;
+    }
+}
